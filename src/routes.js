f181a0b993f5be1e5e3cae56a1a01fe758069df3
Kaua Fortes <kaua.fortes03@gmail.com>
2022-07-19 21:19:44 -0300

    19/07

--- src/routes.js
+++ src/routes.js
@@ -10,8 +10,15 @@ const LessonsController = require('./controllers/LessonsController');
 routes.get('/', IndexController.index);
 
 // rotas para cursos
+
+//cria o curso
 routes.post('/courses', CoursesController.create);
+//busca todos os cursos
 routes.get('/courses', CoursesController.find);
+//busca um curso pelo id
+routes.get('/courses/:id', CoursesController.findById)
+
+
 
 routes.post('/instructors', InstructorsController.create);
 

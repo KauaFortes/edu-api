9ccd6d0674920096646f677ea6637abd550f9512
Kaua Fortes <kaua.fortes03@gmail.com>
2022-07-21 21:28:46 -0300

    21/07

---
+++ requests.js
@@ -0,0 +1,15 @@
+/*const urlAPI = 'https://kb-newdev.herokuapp.com'
+
+async function getCourses() {
+  const response = await fetch(`${urlAPI}/courses`, {
+    method: 'POST',
+    body: {
+      test: 'valor'
+    }
+  })
+
+  const dataJson = await response.json()
+  console.log(dataJson)
+}
+
+getCourses()*/
